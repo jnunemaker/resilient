86c5d6c6e8138a7dcb3cb12e8793e6a53f154bbe
John Nunemaker <nunemaker@gmail.com>
2015-12-07 15:55:51 -0500

    Add default and set rolling config tests

--- lib/resilient/circuit_breaker/rolling_config.rb
+++ lib/resilient/circuit_breaker/rolling_config.rb
@@ -10,11 +10,11 @@ module Resilient
       attr_reader :bucket_size_in_seconds
 
       def initialize(options = {})
-        @error_threshold_percentage = options.fetch(:error_threshold_percentage, 50)
-        @sleep_window_seconds = options.fetch(:sleep_window_seconds, 5)
-        @request_volume_threshold = options.fetch(:request_volume_threshold, 20)
         @force_open = options.fetch(:force_open, false)
         @force_closed = options.fetch(:force_closed, false)
+        @sleep_window_seconds = options.fetch(:sleep_window_seconds, 5)
+        @request_volume_threshold = options.fetch(:request_volume_threshold, 20)
+        @error_threshold_percentage = options.fetch(:error_threshold_percentage, 50)
         @number_of_buckets = options.fetch(:number_of_buckets, 6)
         @bucket_size_in_seconds = options.fetch(:bucket_size_in_seconds, 10)
       end
--- test/resilient/circuit_breaker/rolling_config_test.rb
+++ test/resilient/circuit_breaker/rolling_config_test.rb
@@ -9,6 +9,62 @@ module Resilient
       end
 
       include RollingConfigInterfaceTest
+
+      def test_defaults_force_open
+        assert_equal false, @object.force_open
+      end
+
+      def test_allows_overriding_force_open
+        assert_equal true, RollingConfig.new(force_open: true).force_open
+      end
+
+      def test_defaults_force_closed
+        assert_equal false, @object.force_closed
+      end
+
+      def test_allows_overriding_force_closed
+        assert_equal true, RollingConfig.new(force_closed: true).force_closed
+      end
+
+      def test_defaults_sleep_window_seconds
+        assert_equal 5, @object.sleep_window_seconds
+      end
+
+      def test_allows_overriding_sleep_window_seconds
+        assert_equal 2, RollingConfig.new(sleep_window_seconds: 2).sleep_window_seconds
+      end
+
+      def test_defaults_request_volume_threshold
+        assert_equal 20, @object.request_volume_threshold
+      end
+
+      def test_allows_overriding_request_volume_threshold
+        assert_equal 1, RollingConfig.new(request_volume_threshold: 1).request_volume_threshold
+      end
+
+      def test_defaults_error_threshold_percentage
+        assert_equal 50, @object.error_threshold_percentage
+      end
+
+      def test_allows_overriding_error_threshold_percentage
+        assert_equal 12, RollingConfig.new(error_threshold_percentage: 12).error_threshold_percentage
+      end
+
+      def test_defaults_number_of_buckets
+        assert_equal 6, @object.number_of_buckets
+      end
+
+      def test_allows_overriding_number_of_buckets
+        assert_equal 8, RollingConfig.new(number_of_buckets: 8).number_of_buckets
+      end
+
+      def test_defaults_bucket_size_in_seconds
+        assert_equal 10, @object.bucket_size_in_seconds
+      end
+
+      def test_allows_overriding_bucket_size_in_seconds
+        assert_equal 111, RollingConfig.new(bucket_size_in_seconds: 111).bucket_size_in_seconds
+      end
     end
   end
 end
