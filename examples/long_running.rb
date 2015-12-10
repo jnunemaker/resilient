fb36549dcfa4aa8235f15e17df3bccc559fd47db
John Nunemaker <nunemaker@gmail.com>
2015-12-10 11:35:17 -0500

    Add long running example

---
+++ examples/long_running.rb
@@ -0,0 +1,42 @@
+# setting up load path
+require "pathname"
+root_path = Pathname(__FILE__).dirname.join("..").expand_path
+lib_path  = root_path.join("lib")
+$:.unshift(lib_path)
+
+# requiring stuff for this example
+require "pp"
+require "resilient/circuit_breaker"
+
+config = Resilient::CircuitBreaker::Config.new({
+  sleep_window_seconds: 5,
+  request_volume_threshold: 20,
+  error_threshold_percentage: 10,
+  number_of_buckets: 10,
+  bucket_size_in_seconds: 1,
+})
+circuit_breaker = Resilient::CircuitBreaker.new(config: config)
+
+iterations = 0
+loop do
+  if circuit_breaker.allow_request?
+    begin
+      puts "request allowed"
+      raise if rand(100) < 10
+      puts "request succeeded"
+      circuit_breaker.mark_success
+    rescue => boom
+      puts "request failed"
+      circuit_breaker.mark_failure
+    end
+  else
+    puts "request denied"
+  end
+  puts "\n"
+  sleep 0.1
+  iterations += 1
+
+  if iterations % 10 == 0
+    p successes: circuit_breaker.metrics.successes, failures: circuit_breaker.metrics.failures, error_percentage: circuit_breaker.metrics.error_percentage, buckets: circuit_breaker.metrics.buckets.length
+  end
+end
