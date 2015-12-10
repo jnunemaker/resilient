0ff864a693fb3c41f1045dce050dab75d0262166
John Nunemaker <nunemaker@gmail.com>
2015-12-10 13:57:34 -0500

    Add docs to properties

--- lib/resilient/circuit_breaker/properties.rb
+++ lib/resilient/circuit_breaker/properties.rb
@@ -3,13 +3,32 @@ require "resilient/instrumenters/noop"
 module Resilient
   class CircuitBreaker
     class Properties
+      # allows forcing the circuit open (stopping all requests)
       attr_reader :force_open
+
+      # allows ignoring errors and therefore never trip "open"
+      # (ie. allow all traffic through); normal instrumentation will still
+      # happen, thus allowing you to "test" configuration live without impact
       attr_reader :force_closed
+
+      # what to use to instrument all events that happen
+      # (ie: ActiveSupport::Notifications)
       attr_reader :instrumenter
+
+      # seconds after tripping circuit before allowing retry
       attr_reader :sleep_window_seconds
+
+      # number of requests that must be made within a statistical window before
+      # open/close decisions are made using stats
       attr_reader :request_volume_threshold
+
+      # % of "marks" that must be failed to trip the circuit
       attr_reader :error_threshold_percentage
+
+      # number of buckets in the statistical window
       attr_reader :number_of_buckets
+
+      # size of buckets in statistical window
       attr_reader :bucket_size_in_seconds
 
       def initialize(options = {})
