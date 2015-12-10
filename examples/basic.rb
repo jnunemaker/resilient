d884fae5100830b4ef2a2c7a00d9441e92752b03
John Nunemaker <nunemaker@gmail.com>
2015-12-09 20:02:33 -0500

    Add basic example

---
+++ examples/basic.rb
@@ -0,0 +1,81 @@
+# setting up load path
+require "pathname"
+root_path = Pathname(__FILE__).dirname.join("..").expand_path
+lib_path  = root_path.join("lib")
+$:.unshift(lib_path)
+
+# requiring stuff for this example
+require "pp"
+require "resilient/circuit_breaker"
+
+config = Resilient::CircuitBreaker::RollingConfig.new({
+  sleep_window_seconds: 1,
+  request_volume_threshold: 10,
+  error_threshold_percentage: 25,
+})
+circuit_breaker = Resilient::CircuitBreaker.new(config: config)
+
+# success
+if circuit_breaker.allow_request?
+  begin
+    puts "do expensive thing"
+    circuit_breaker.mark_success
+  rescue => boom
+    # won't get here in this example
+    circuit_breaker.mark_failure
+  end
+else
+  raise "will not get here"
+end
+
+# failure
+if circuit_breaker.allow_request?
+  begin
+    raise
+  rescue => boom
+    circuit_breaker.mark_failure
+    puts "failed slow, do fallback"
+  end
+else
+  raise "will not get here"
+end
+
+# trip circuit, imagine this being same as above but in real life...
+# also, we have to fail at least the request volume threshold number of times
+circuit_breaker.config.request_volume_threshold.times do
+  circuit_breaker.mark_failure
+end
+
+# fail fast
+if circuit_breaker.allow_request?
+  raise "will not get here"
+else
+  puts "failed fast, do fallback"
+end
+
+now = Time.now
+
+while Time.now - now < 3
+  if circuit_breaker.allow_request?
+    puts "doing a single attempt as we've failed fast for sleep_window_seconds"
+    break
+  else
+    puts "failed fast, do fallback"
+  end
+  sleep rand(0.1)
+end
+
+if circuit_breaker.allow_request?
+  raise "will not get here"
+else
+  puts "request denied because single request has not been marked success yet"
+end
+
+puts "marking single request as success"
+circuit_breaker.mark_success
+
+if circuit_breaker.allow_request?
+  puts "circuit reset and back closed now, allowing requests"
+else
+  raise "will not get here"
+end
